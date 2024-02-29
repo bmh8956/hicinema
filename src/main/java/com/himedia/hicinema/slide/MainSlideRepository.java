bd0385b6abba77e4c65a7cd8d640fdacb055628c
KIM <nakyung2371@gmail.com>
2024-02-29 20:11:20 +0900

    02.29 mainslide-knk

--- src/main/java/com/himedia/hicinema/slide/MainSlideRepository.java
+++ src/main/java/com/himedia/hicinema/slide/MainSlideRepository.java
@@ -2,7 +2,7 @@ package com.himedia.hicinema.slide;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MainSlideRepository  extends JpaRepository<MainSlide, Long> {
-    MainSlide findByImgId(String imgId);
+public interface MainSlideRepository {
+
 
 }
--- src/test/java/com/himedia/hicinema/HicinemaApplicationTests.java
+++ src/test/java/com/himedia/hicinema/HicinemaApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Queue;
 
@@ -21,18 +22,14 @@ class HicinemaApplicationTests {
 
 //	@Autowired
 //	private MainSlideRepository mainSlideRepository;
-
-
-//	void testJpa() {
-//		MainSlide m1 = new MainSlide();
-//		m1.setImgId("1-1");
-//		m1.setVideoId("1-2");
-//		this.mainSlideRepository.save(m1);
 //
-//		MainSlide m2 = new MainSlide();
-//		m2.setImgId("2-1");
-//		m2.setVideoId("2-2");
-//		this.mainSlideRepository.save(m2);
+//	@Test
+//	void testJpa() {
+//		List<MainSlide> all = this.mainSlideRepository.findAll();
+//		assertEquals(2, all.size());
+//
+//		MainSlide m = all.get(0);
+//		assertEquals("1-1", m.getImgId());
 //	}
 
 
