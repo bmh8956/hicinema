835d8b18075bf77de70c2bfe3dc9015fc9b7a6ce
KIM NAKYUNG <nakyung2371@gmail.com>
2024-03-18 23:24:51 +0900

    03.18 notice

--- src/main/java/com/himedia/hicinema/notice/AdminNoticeController.java
+++ src/main/java/com/himedia/hicinema/notice/AdminNoticeController.java
@@ -1,5 +1,6 @@
 package com.himedia.hicinema.notice;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,25 +10,32 @@ import com.himedia.hicinema.movie.AdminMovieController;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/admin/notice")
+
 public class AdminNoticeController {
-	@GetMapping("/notice_list")
-	public String eventList(Model model) {
-		model.addAttribute("title", "이벤트 리스트");
-		return "admin/event/event_list";
-	}
-	
 	@GetMapping("/notice_form")
-	public String eventForm(Model model) {
-		model.addAttribute("title", "이벤트 리스트");
-		return "admin/event/event_list";
+	public String eventList(Model model) {
+		model.addAttribute("title", "공지사항 등록");
+		return "admin/notice/notice_form";
 	}
-	
+
+	private final NoticeRepository noticeRepository;
+	@GetMapping("/notice_list")
+	public String list(Model model) {
+		List<Notice> noticeList = this.noticeRepository.findAll();
+		model.addAttribute("title", "공지사항 목록");
+		model.addAttribute("noticeList", noticeList);
+		return "admin/notice/notice_list";
+	}
+
 	@GetMapping("/notice_detail")
 	public String eventDetail(Model model) {
-		model.addAttribute("title", "이벤트 리스트");
-		return "admin/event/event_list";
+		model.addAttribute("title", "공지사항 수정");
+		return "admin/notice/notice_detail";
 	}
 }
